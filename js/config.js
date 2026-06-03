013b74ceaa292c27bd28e4ece27909b754524979
aannie-0420 <aannie0420@gmail.com>
2026-06-03 16:30:20 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDxDD9jZA25Hk19HF0UUo0f3ZiEWv1azuk",
+    authDomain: "advantech-shuttle-tracker.firebaseapp.com",
+    databaseURL: "https://advantech-shuttle-tracker-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "advantech-shuttle-tracker",
+    storageBucket: "advantech-shuttle-tracker.firebasestorage.app",
+    messagingSenderId: "749819334514",
+    appId: "1:749819334514:web:582850faa8216c5b7d57eb"
+};
